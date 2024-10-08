e161e941caf267b96acbb5fcbdb54893d118d3b1
Slobodan Starcevic <slobodanstarcevicode@gmail.com>
2024-10-08 22:08:14 +0200

    text done on section 1

---
+++ src/utils/links.js
@@ -0,0 +1,7 @@
+export const links = {
+    home: '/',
+    events: '/events',
+    gallery: '/gallery',
+    tickets: 'https://www.eventbrite.com.au/e/last-night-on-earth-ft-mr-loveless-tickets-1020325520097?utm-campaign=social&utm-content=attendeeshare&utm-medium=discovery&utm-term=listing&utm-source=cp&aff=ebdsshcopyurl',
+    merch: 'https://mrloveless.store/',
+}
