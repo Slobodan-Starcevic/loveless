8b35547450553e00f83bd28c0f082c7c603eedb2
Slobodan Starcevic <slobodanstarcevicode@gmail.com>
2024-10-11 22:53:39 +0200

    instagram done

--- src/utils/links.js
+++ src/utils/links.js
@@ -4,4 +4,6 @@ export const links = {
     gallery: '/gallery',
     tickets: 'https://www.eventbrite.com.au/e/last-night-on-earth-ft-mr-loveless-tickets-1020325520097?utm-campaign=social&utm-content=attendeeshare&utm-medium=discovery&utm-term=listing&utm-source=cp&aff=ebdsshcopyurl',
     merch: 'https://mrloveless.store/',
+    instagram: 'https://www.instagram.com/mrlovelessbrisbane',
+
 }
