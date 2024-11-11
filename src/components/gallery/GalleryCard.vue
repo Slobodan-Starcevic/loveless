c10f9f053c85d2fe77a7891582670978dd6db4bb
Slobodan Starcevic <slobodanstarcevicode@gmail.com>
2024-11-11 19:03:08 +0100

    responsive gallery and cards made

---
+++ src/components/gallery/GalleryCard.vue
@@ -0,0 +1,68 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+<div class="card-wrapper">
+    <img src="images/MrLoveless90s-153.jpg" alt="">
+    <div class="card-content">
+        <p class="card-title">Halloween Party</p>
+        <p>Saturday 26th October 2024</p>
+    </div>
+</div>
+</template>
+
+<style lang="scss" scoped>
+.card-wrapper{
+    position: relative;
+    width: calc(33% - var(--spacing-2xl));
+    min-width: 300px;
+    height: auto;
+    max-height: 600px;
+    border-radius: var(--radius-l);
+    box-shadow: 17px 12px 36px var(--loveless-black);
+    overflow: hidden;
+    display: flex;
+    justify-content: center;
+    align-items: center; 
+    img{
+        width: 100%;
+        height: 100%;
+        object-fit: cover;
+    }
+
+    .card-content{
+        position: absolute;
+        bottom: 0;
+        width: 100%;
+        height: max-content;
+        display: flex;
+        flex-direction: column;
+        gap: var(--spacing-s);
+        padding: var(--spacing-l);
+        background-color: var(--loveless-pink);
+        color: var(--loveless-black);
+        .card-title{
+            font-family: 'Mortend';
+            font-size: var(--font-size-xl);
+        }
+    }
+}
+
+@media screen and (max-width : 767px) {
+    .card-wrapper{
+        width: 90% !important;
+        min-width: 0 !important;
+        max-height: 100vh;
+    }
+}
+@media screen and (min-width: 768px) and (max-width: 1079px) {
+    .card-wrapper{
+        width: calc(50% - var(--spacing-2xl)) !important;
+        min-width: 0 !important;
+    }
+}
+@media screen and (min-width : 1080px) {
+    
+}
+</style>
---
+++ src/components/gallery/ImageGallery.vue
@@ -0,0 +1,43 @@
+<script setup lang="ts">
+import GalleryCard from './GalleryCard.vue';
+
+
+</script>
+
+<template>
+  <div class="gallery-cards-wrapper">
+    <GalleryCard/>
+    <GalleryCard/>
+    <GalleryCard/>
+    <GalleryCard/>
+    <GalleryCard/>
+    <GalleryCard/>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+  .gallery-cards-wrapper{
+    width: 100%;
+    max-width: 1600px;
+    height: max-content;
+    margin: 0 auto;
+    padding: var(--spacing-2xl);
+    display: flex;
+    flex-wrap: wrap;
+    align-items: center;
+    justify-content: center;
+    gap: var(--spacing-2xl);
+  }
+
+  @media screen and (max-width : 767px) {
+    .gallery-cards-wrapper{
+      padding: 0;
+    }
+  }
+  @media screen and (min-width: 768px) and (max-width: 1079px) {
+      
+  }
+  @media screen and (min-width : 1080px) {
+      
+  }
+  </style>
