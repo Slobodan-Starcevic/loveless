62dec1fd61ae723e9d65bcf2aaecc63a2c717b5a
Slobodan Starcevic <slobodanstarcevicode@gmail.com>
2024-11-17 20:38:43 +0100

    whats on logic

--- src/components/landing/WhatsOnScroller.vue
+++ src/components/landing/WhatsOnScroller.vue
@@ -1,46 +1,67 @@
 <script setup lang="ts">
-import { onMounted } from 'vue';
+import { computed } from 'vue';
+import type { EventProps } from '../../types/event'
 
-onMounted(() => {
-    const scrollers = document.querySelectorAll<HTMLElement>(".scroller");
-    scrollers.forEach((scroller) => {
-        const scrollerInner = scroller.querySelector<HTMLElement>(".scroller-inner");
-        if(scrollerInner){
-            const scrollerContent = Array.from(scrollerInner.children) as HTMLElement[];
+const props = defineProps<{
+    events: Array<EventProps>
+}>();
 
-            scrollerContent.forEach((item) => {
-            const duplicatedItem = item.cloneNode(true) as HTMLElement;
-            duplicatedItem.setAttribute("aria-hidden", "true");
-            scrollerInner.appendChild(duplicatedItem);
-            });
-        }
-    });
-})
+function cycleToTarget(array: string[], targetLength: number): string[] {
+    const result = [];
+    for (let i = 0; i < targetLength; i++){
+        result.push(array[i % array.length])
+    }
+    return result;
+}
+
+function getRandomItems(array: string[], count: number): string[]{
+    const shuffled = [...array].sort(() => Math.random() - 0.5)
+    return shuffled.slice(0, count);
+};
+
+const bannerUrls = props.events.map(event => event.data.banner);
+const SCROLLER_SIZE = 8;
+
+const imageArray = computed(() => {
+    if ( props.events.length == 0){
+        return Array(SCROLLER_SIZE).fill('/images/poster.png');
+    }
+    if ( props.events.length == 1){
+        return Array(SCROLLER_SIZE).fill(props.events[0].data.banner);
+    }
+    if ( props.events.length >= SCROLLER_SIZE){
+        return getRandomItems(bannerUrls, SCROLLER_SIZE);
+    }
+
+    return cycleToTarget(bannerUrls, SCROLLER_SIZE);
+});
 </script>
 
 <template>
 <div class="scroller-wrapper desktop-content">
                 <div class="scroller">
                     <div class="scroller-inner">
-                        <img src="/images/poster.png" alt="" loading="lazy">
-                        <img src="/images/poster.png" alt="" loading="lazy">
-                        <img src="/images/poster.png" alt="" loading="lazy">
-                        <img src="/images/poster.png" alt="" loading="lazy">
+                        <img v-for="(image, index) in imageArray" 
+                        :key="'scroll1-' + index"
+                        :src="image" 
+                        alt="" 
+                        loading="lazy">
                     </div>
                 </div>
                 <div class="still">
                     <div class="still-inner">
+                        <img :src="imageArray[0]" alt="" loading="lazy">
                         <img src="/images/poster.png" alt="" loading="lazy">
-                        <img src="/images/poster.png" alt="" loading="lazy">
-                        <img src="/images/poster.png" alt="" loading="lazy">
+                        <img :src="imageArray[1]" alt="" loading="lazy">
                     </div>
                 </div>
                 <div class="scroller" data-direction="down">
                     <div class="scroller-inner">
-                        <img src="/images/poster.png" alt="" loading="lazy">
-                        <img src="/images/poster.png" alt="" loading="lazy">
-                        <img src="/images/poster.png" alt="" loading="lazy">
-                        <img src="/images/poster.png" alt="" loading="lazy">
+                        <img v-for="(image, index) in imageArray" 
+                        :key="'scroll2-' + index"
+                        :src="image" 
+                        alt="" 
+                        loading="lazy">
                     </div>
                 </div>
             </div>
