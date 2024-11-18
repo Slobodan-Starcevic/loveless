3cf908e9934ce8b2492732d1a5425e0cb9e614c9
Slobodan Starcevic <slobodanstarcevicode@gmail.com>
2024-11-18 17:17:01 +0100

    dynamic hero and whatson fix

---
+++ src/utils/links.ts
@@ -0,0 +1,7 @@
+export const links: Record<string, string> = {
+    home: '/',
+    events: '/events',
+    gallery: '/gallery',
+    merch: 'https://mrloveless.store/',
+    instagram: 'https://www.instagram.com/mrlovelessbrisbane'
+}
