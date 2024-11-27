8273efac2b5c63aac6ff79fff176051792bc3b9f
Slobodan Starcevic <slobodanstarcevicode@gmail.com>
2024-11-27 12:30:11 +0100

    done? 4

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,19 @@
 import { defineConfig } from 'astro/config';
 import vue from '@astrojs/vue';
 
+import playformInline from '@playform/inline';
+
+import playformCompress from '@playform/compress';
+
+import playformFormat from '@playform/format';
+
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
+    site: 'https://mrloveless.netlify.app/',
     devToolbar: { enabled: false },
-    integrations: [vue()],
+    integrations: [vue(), sitemap(), playformInline({}), playformFormat({}), playformCompress()],
     vite: {
         css: {
           preprocessorOptions: {
